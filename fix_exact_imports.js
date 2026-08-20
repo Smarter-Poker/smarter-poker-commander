03d74694ffd2c36312d0cc0ee7906098fab96e0b
Smarter-Poker <254329056+Smarter-Poker@users.noreply.github.com>
2026-08-20 09:20:46 -0500

    chore: clean up temporary agent scripts

--- fix_exact_imports.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('pages/commander/login.js', 'utf8');
-
-code = code.replace(
-  "import { Loader2, Eye, EyeOff, ArrowRight, Mail, Lock, ChevronRight, UserPlus, ShieldCheck, AlertCircle, Check } from 'lucide-react';",
-  "import { Loader2, Check } from 'lucide-react';"
-);
-
-fs.writeFileSync('pages/commander/login.js', code);
